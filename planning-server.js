389b0c228cf80244b289c6b017312d9d010f5897
Lisa Pama <lisa.pama@gmail.com>
2016-03-21 10:49:27 +0100

    test

--- planning-server.js
+++ planning-server.js
@@ -8,7 +8,6 @@ var path = require('path');
 app.set("views", path.join(__dirname, 'public'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
-//app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/planning', function () {
@@ -17,19 +16,12 @@ app.get('/planning', function () {
 
 app.listen(process.env.PORT);
 
-module.exports=app;
-
-/*app.listen(3000, function () {
-  console.log('port 3000: Planning app');
-});*/
-
-/* var planSchema =  new mongoose.Schema ({
+var planSchema =  new mongoose.Schema ({
    minutes: { type: Number, default:''},
    task: { type: String, default:''}
  });
 
 var Plan = mongoose.model('Plan', planSchema);
-
 
   app.get('/plans', function (req, res) {
     Plan.find(function (err, plans) {
@@ -55,4 +47,5 @@ var Plan = mongoose.model('Plan', planSchema);
         res.status(200).end();
     });
   });
-*/
+
+module.exports=app;
